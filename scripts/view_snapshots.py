fd919c47acfc3aa44db3cd65cddce98fb69a2a1a
Benjamin Gallusser <bgallusser@googlemail.com>
2020-12-11 13:25:01 -0500

    Add interval to multi-snapshot visualization

--- scripts/view_snapshots.py
+++ scripts/view_snapshots.py
@@ -3,46 +3,68 @@ import os
 import random
 import logging
 import subprocess
+import re
+import sys
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '--path',
     '-p',
     type=str,
-    help="The path with the snapshots to show")
+    help="The path with the snapshots to show.")
 parser.add_argument(
     '--datasets',
     '-d',
     type=str,
     nargs='+',
-    help="The datasets in the containers")
+    help="The datasets in the containers.")
 parser.add_argument(
     '--serve',
     '-s',
     action='store_true',
-    help='Serve neuroglancer on public IP'
+    help='Serve neuroglancer on public IP.'
 )
 parser.add_argument(
     '--number',
     '-n',
     type=int,
     default=10,
-    help='show n snapshots at random'
+    help='Show n snapshots at random.'
+)
+parser.add_argument(
+    '--start',
+    type=int,
+    default=0,
+    help="Lower limit iteration for random sampling."
+)
+parser.add_argument(
+    '--stop',
+    type=int,
+    default=sys.maxsize,
+    help='Upper limit iteration for random sampling.'
 )
 
 args = parser.parse_args()
 
+regex = r'(\d+)\.hdf'
 snapshots = [
     os.path.join(args.path, f)
     for f in os.listdir(args.path)
-    if f.endswith('.hdf')
+    if re.search(regex, f)
 ]
 
-snapshots = random.sample(snapshots, min(args.number, len(snapshots)))
+snapshots = [
+    s for s in snapshots
+    if int(re.search(regex, s)[1]) >= args.start
+    and int(re.search(regex, s)[1]) <= args.stop
+]
+
+snapshots = sorted(random.sample(snapshots, min(args.number, len(snapshots))))
 logger.debug(f'snapshots: {snapshots}')
 
 subprocesses = []
@@ -59,6 +81,7 @@ for s in snapshots:
     logger.debug(' '.join(cmd))
     proc = subprocess.Popen(' '.join(cmd), shell=True, stdin=subprocess.PIPE)
     subprocesses.append(proc)
+    time.sleep(0.5)
 
 
 input()
